d6bc25b7aa73d25c0cdfbee594872c4f08bc459b
lorandg <guillaume.lorand@gmail.com>
2020-09-16 17:16:05 +0200

    fix invalid url

--- js/ride.js
+++ js/ride.js
@@ -18,7 +18,7 @@ WildRydes.map = WildRydes.map || {};
     function requestUnicorn(pickupLocation) {
         $.ajax({
             method: 'POST',
-            url: _config.api.invokeUrl + '/ride-08',
+            url: _config.api.invokeUrl + '/ride-user08',
             headers: {
                 Authorization: authToken
             },
